dc3192fa255aeb9eddd717bd23f63645fe794a04
Каte <mle-user@epd6g5549tfs4pafk7uj.auto.internal>
2024-02-17 13:13:19 +0000

    changed pipeline

--- depricated/churn.py
+++ depricated/churn.py
@@ -1,5 +1,10 @@
+# dags/churn.py
+
 import pendulum
 from airflow.decorators import dag, task
+from sqlalchemy import MetaData, Table, Column, String, Integer, Float, UniqueConstraint, DateTime
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pandas as pd
 
 @dag(
     schedule='@once',
@@ -8,60 +13,60 @@ from airflow.decorators import dag, task
     tags=["ETL"]
 )
 def prepare_churn_dataset():
-    import pandas as pd
-    import numpy as np
-    from airflow.providers.postgres.hooks.postgres import PostgresHook
+
     @task()
     def create_table():
-        import sqlalchemy
-        from sqlalchemy import MetaData, Table, Column, String, Integer, inspect, Float, UniqueConstraint, DateTime
-        hook = PostgresHook('destination_db')
-        db_conn = hook.get_sqlalchemy_engine()
+        from sqlalchemy import MetaData, Table, Column, String, Integer, Float, UniqueConstraint, DateTime
         metadata = MetaData()
+        hook = PostgresHook('destination_db')
+        engine = hook.get_sqlalchemy_engine()
+
         users_churn = Table(
-            'users_churn', 
+            'users_churn',
             metadata,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('customer_id', String),
+            Column('id', Integer, primary_key=True),
+            Column('customer_id', String, unique=True),
             Column('begin_date', DateTime),
             Column('end_date', DateTime),
             Column('type', String),
-            Column('paperless_billing', String),
+            Column('paperless_billing', Integer),
             Column('payment_method', String),
             Column('monthly_charges', Float),
             Column('total_charges', Float),
             Column('internet_service', String),
-            Column('online_security', String),
-            Column('online_backup', String),
-            Column('device_protection', String),
-            Column('tech_support', String),
-            Column('streaming_tv', String),
-            Column('streaming_movies', String),
+            Column('online_security', Integer),
+            Column('online_backup', Integer),
+            Column('device_protection', Integer),
+            Column('tech_support', Integer),
+            Column('streaming_tv', Integer),
+            Column('streaming_movies', Integer),
             Column('gender', String),
             Column('senior_citizen', Integer),
-            Column('partner', String),
-            Column('dependents', String),
-            Column('multiple_lines', String),
+            Column('partner', Integer),
+            Column('dependents', Integer),
+            Column('multiple_lines', Integer),
             Column('target', Integer),
-            UniqueConstraint('customer_id', name='unique_customer_id_constraint')
-        )  
-        if not inspect(db_conn).has_table(users_churn.name): 
-            metadata.create_all(db_conn)
+            UniqueConstraint('customer_id', name='uq_customer_id')
+        )
+
+        metadata.create_all(engine)
+
+    create_table()
+
     @task()
     def extract(**kwargs):
-
         hook = PostgresHook('source_db')
         conn = hook.get_conn()
         sql = f"""
-        select
+        SELECT
             c.customer_id, c.begin_date, c.end_date, c.type, c.paperless_billing, c.payment_method, c.monthly_charges, c.total_charges,
             i.internet_service, i.online_security, i.online_backup, i.device_protection, i.tech_support, i.streaming_tv, i.streaming_movies,
             p.gender, p.senior_citizen, p.partner, p.dependents,
             ph.multiple_lines
-        from contracts as c
-        left join internet as i on i.customer_id = c.customer_id
-        left join personal as p on p.customer_id = c.customer_id
-        left join phone as ph on ph.customer_id = c.customer_id
+        FROM contracts as c
+        LEFT JOIN internet as i ON i.customer_id = c.customer_id
+        LEFT JOIN personal as p ON p.customer_id = c.customer_id
+        LEFT JOIN phone as ph ON ph.customer_id = c.customer_id
         """
         data = pd.read_sql(sql, conn)
         conn.close()
@@ -82,11 +87,8 @@ def prepare_churn_dataset():
             target_fields=data.columns.tolist(),
             replace_index=['customer_id'],
             rows=data.values.tolist()
-    )
+        )
 
-    create_table()
     data = extract()
     transformed_data = transform(data)
     load(transformed_data)
-
-prepare_churn_dataset()
